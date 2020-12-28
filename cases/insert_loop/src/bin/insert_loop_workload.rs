514b4d13bd4183a914b7561e0e1cbb78ce4bd379
David Cook <divergentdave@gmail.com>
2020-12-28 17:05:58 -0600

    Use SIGKILL instead of std::process::exit()

--- cases/insert_loop/src/bin/insert_loop_workload.rs
+++ cases/insert_loop/src/bin/insert_loop_workload.rs
@@ -15,7 +15,9 @@ fn start_timer() {
     thread::spawn(|| {
         let runtime = rand::thread_rng().gen_range(0, 60);
         thread::sleep(Duration::from_millis(runtime));
-        process::exit(9);
+        unsafe {
+            libc::raise(9);
+        }
     });
 }
 
@@ -61,19 +63,33 @@ fn main() {
                 } else {
                     break;
                 }
+            } else if child == -1 {
+                eprintln!("fork failed, errno is {}", unsafe {
+                    *libc::__errno_location()
+                });
+                process::exit(1);
             } else {
                 let mut status: libc::c_int = 0;
-                unsafe {
-                    libc::waitpid(child, &mut status as *mut libc::c_int, 0);
+                let rv = unsafe { libc::waitpid(child, &mut status as *mut libc::c_int, 0) };
+                if rv == -1 {
+                    eprintln!("waitpid failed, errno is {}", unsafe {
+                        *libc::__errno_location()
+                    });
+                    process::exit(1);
                 }
-                match (libc::WIFEXITED(status), libc::WEXITSTATUS(status)) {
-                    (true, 9) => continue,
-                    (true, 0) => break,
-                    (true, exit_status) => {
+                match (
+                    libc::WIFEXITED(status),
+                    libc::WEXITSTATUS(status),
+                    libc::WIFSIGNALED(status),
+                    libc::WTERMSIG(status),
+                ) {
+                    (true, 0, _, _) => break,
+                    (true, exit_status, _, _) => {
                         eprintln!("child exited with status {}", exit_status);
                         process::exit(1);
                     }
-                    (false, _) => {
+                    (_, _, true, 9) => continue,
+                    _ => {
                         eprintln!("child exited abnormally");
                         process::exit(1);
                     }
