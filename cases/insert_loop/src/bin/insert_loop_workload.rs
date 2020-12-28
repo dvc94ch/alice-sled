d9ba6b84a77490e84e7d2bf6bce666f30877caf8
David Cook <divergentdave@gmail.com>
2020-12-28 11:49:14 -0600

    Use clap to parse arguments

--- cases/insert_loop/src/bin/insert_loop_workload.rs
+++ cases/insert_loop/src/bin/insert_loop_workload.rs
@@ -1,6 +1,6 @@
-use std::env;
 use std::process;
 
+use clap::{App, Arg};
 use rand::Rng;
 
 use sled_workload_insert_loop::*;
@@ -9,13 +9,19 @@ const DB_DIR: &str = "workload_dir";
 const DEFAULT_LOOP_COUNT: usize = CYCLE * 10;
 
 fn main() -> Result<(), sled::Error> {
-    let loop_count = if let Some(argument) = env::args().skip(1).next() {
-        if argument.is_empty() {
-            DEFAULT_LOOP_COUNT
-        } else if let Ok(loop_count) = argument.parse() {
+    let matches = App::new("insert_loop_workload")
+        .arg(
+            Arg::with_name("loop_count")
+                .takes_value(true)
+                .index(1)
+                .required(false)
+        )
+        .get_matches();
+    let loop_count = if let Some(loop_count) = matches.value_of("loop_count") {
+        if let Ok(loop_count) = loop_count.parse() {
             loop_count
         } else {
-            eprintln!("Could not parse loop count argument");
+            eprintln!("{}", matches.usage());
             process::exit(1);
         }
     } else {
